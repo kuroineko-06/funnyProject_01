0d68a0182d21e492e3d469353d98ba33f3aa206d
kuroineko-06 <huy12060301@gmail.com>
2024-01-12 14:38:29 +0700

    neko commit

--- demo_app_1/lib/featured/pages/home_pages.dart
+++ demo_app_1/lib/featured/pages/home_pages.dart
@@ -134,7 +134,7 @@ class _HomeState extends State<Home> {
                             ),
                           ),
                           SizedBox(
-                            height: 150,
+                            height: 180,
                             child: new ListView.builder(
                               scrollDirection: Axis.horizontal,
                               shrinkWrap: true,
